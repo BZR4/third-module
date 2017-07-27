3e276861136bdcd14b6483b5b907338eada8b160
Esdras B Silva <bzr4@icloud.com>
2017-07-27 12:38:41 -0300

    Insercao de funcionalidadedo botao OK na mensagem.

--- app/src/main/java/com/example/esdras/myapplication/MainActivity.java
+++ app/src/main/java/com/example/esdras/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.esdras.myapplication;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +16,12 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder alert = new AlertDialog.Builder(this)
                 .setTitle("Alert")
                 .setMessage("Hello World!")
-                .setPositiveButton("Ok",null);
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_LONG).show();
+                    }
+                });
                 alert.create()
                 .show();
 
